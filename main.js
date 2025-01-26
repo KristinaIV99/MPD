7debe4e385456e30aae702835317e567b7991725
KristinaIV99 <ivancova.k@gmail.com>
2025-01-26 21:18:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { TextReader } from './text-reader.js';
+import TextReader from './text-reader.js';
 import Logger from './logger.js' // Default export
 import { LOG_LEVELS } from './logger.js' // Named export
 import DOMPurify from './vendor/purify.es.mjs';
