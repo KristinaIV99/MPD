7b7312018bbfa86a9e3a53e2f4a433c0a0f76888
KristinaIV99 <ivancova.k@gmail.com>
2025-01-26 20:34:50 +0100

    Update text-reader.js

--- text-reader.js
+++ text-reader.js
@@ -1,5 +1,5 @@
 import { marked } from './vendor/marked.esm.js';
-import DOMPurify from './vendor/purify.es.mjs';
+import { createDOMPurify } from './vendor/purify.es.mjs';
 import Logger from './logger.js';
 import { LOG_LEVELS } from './logger.js';
 
@@ -23,7 +23,7 @@ export class TextReader {
      ...config
    };
 
-   this.DOMPurify = DOMPurify;
+   this.DOMPurify = createDOMPurify(window);
    this.config.logger.error('DOMPurify initialized:', this.DOMPurify);
    
    this.abortController = new AbortController();
