3398b753466b6c81b24f137611a160384634293b
KristinaIV99 <ivancova.k@gmail.com>
2025-01-27 20:30:30 +0100

    Update word-counter.js

--- word-counter.js
+++ word-counter.js
@@ -1,10 +1,16 @@
 // word-counter.js
+console.log('WordCounter modulis užkrautas');
+
 export class WordCounter {
   constructor() {
     this.COUNTER_NAME = '[WordCounter]';
+    console.log(`${this.COUNTER_NAME} Sukurtas`);
   }
 
   countWords(text) {
+    console.log(`${this.COUNTER_NAME} Pradedamas žodžių skaičiavimas`);
+    console.log(`${this.COUNTER_NAME} Gautas tekstas:`, text.slice(0, 100) + '...'); // Parodys pirmus 100 simbolių
+    
     // Išvalome tekstą nuo specialių simbolių ir skaičiuojame žodžius
     const cleanText = text.replace(/[^\w\s]/g, ' ')
                          .replace(/\s+/g, ' ')
