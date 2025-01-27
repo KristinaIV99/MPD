68073ea3d29673c163dd7ce274d98ed3f6c9cdca
KristinaIV99 <ivancova.k@gmail.com>
2025-01-27 20:50:31 +0100

    Update word-counter.js

--- word-counter.js
+++ word-counter.js
@@ -1,4 +1,3 @@
-// word-counter.js
 export class WordCounter {
   constructor() {
     this.COUNTER_NAME = '[WordCounter]';
@@ -8,26 +7,52 @@ export class WordCounter {
   countWords(text) {
     console.log(`${this.COUNTER_NAME} Pradedamas žodžių skaičiavimas`);
     
-    // Pašaliname specialius simbolius ir perteklinius tarpus
     const cleanText = text
-      // Pakeičiame naujų eilučių simbolius tarpais
       .replace(/\n/g, ' ')
-      // Pašaliname Unicode tarpus ir specialius simbolius
       .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g, ' ')
-      // Pakeičiame kelis tarpus vienu
       .replace(/\s+/g, ' ')
-      // Pašaliname tarpus pradžioje ir pabaigoje
       .trim();
     
-    // Skaičiuojame žodžius (tik realius žodžius, ne tuščius tarpus)
     const words = cleanText.split(' ').filter(word => word.length > 0);
     const wordCount = words.length;
     
     console.log(`${this.COUNTER_NAME} Suskaičiuota žodžių: ${wordCount}`);
-    
-    // Patikriname pirmus kelis žodžius (debugginimui)
     console.log(`${this.COUNTER_NAME} Pirmi 5 žodžiai:`, words.slice(0, 5));
     
     return wordCount;
+  } // Fixed: Added closing brace for countWords()
+
+  getWordStatistics(text) { // Fixed: Now a separate method
+    const words = text
+      .toLowerCase()
+      .replace(/\n/g, ' ')
+      .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()
+      .split(' ')
+      .filter(word => word.length > 0);
+
+    const wordFrequency = {};
+    words.forEach(word => {
+      wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+    });
+
+    const mostCommon = Object.entries(wordFrequency)
+      .sort(([, a], [, b]) => b - a)
+      .slice(0, 10);
+
+    console.log(`${this.COUNTER_NAME} Statistika:`);
+    console.log(`- Viso žodžių: ${words.length}`);
+    console.log(`- Unikalių žodžių: ${Object.keys(wordFrequency).length}`);
+    console.log('- Dažniausi žodžiai:');
+    mostCommon.forEach(([word, count]) => {
+      console.log(`  ${word}: ${count} kartų`);
+    });
+
+    return {
+      totalWords: words.length,
+      uniqueWords: Object.keys(wordFrequency).length,
+      mostCommon
+    };
   }
 }
