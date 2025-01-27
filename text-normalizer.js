e1f5c1bc37e10b5d4874692cc8ea24915e244a9d
KristinaIV99 <ivancova.k@gmail.com>
2025-01-27 12:11:35 +0100

    Update text-normalizer.js

--- text-normalizer.js
+++ text-normalizer.js
@@ -1,30 +1,25 @@
-// text-normalizer.js
 export class TextNormalizer {
   constructor(logger) {
     this.logger = logger;
   }
 
-  // Pagrindinė normalizavimo funkcija
   normalizeMarkdown(text) {
     try {
       let normalized = text;
       
-      // 1. Headers
-      normalized = normalized.replace(/^#\s+(.*?)\s*#*$/gm, '# $1');
-      normalized = normalized.replace(/_+/g, '_');  // Sutvarkome visus pabraukimus
-
-      // 2. Citatos ir blokai
+      // 1. Headers - tvarkome tik # kiekį pabaigoje
+      normalized = normalized.replace(/^#+\s+(.*?)\s*#*$/gm, '# $1');
+      
+      // 2. Citatos - normalizuojame į >
       normalized = this.normalizeQuotes(normalized);
       
       // 3. Specialūs simboliai
-      normalized = normalized.replace(/[“”]/g, '"');
-      normalized = normalized.replace(/[‘’]/g, "'");
-      normalized = normalized.replace(/–/g, '-');
+      normalized = normalized.replace(/[“”]/g, '"').replace(/[‘’]/g, "'").replace(/–/g, '-');
       
-      // 4. Emphasis ir escape
+      // 4. Emphasis - tvarkome pirmiausia
       normalized = this.handleEmphasis(normalized);
       
-      // 5. Tarpai ir eilučių tarpai
+      // 5. Tarpai
       normalized = this.cleanWhitespace(normalized);
 
       this.logger.log('Markdown normalized successfully');
@@ -35,18 +30,19 @@ export class TextNormalizer {
     }
   }
 
-  // Privatūs metodai (be # sintaksės)
   normalizeQuotes(text) {
-    return text
-      .replace(/(\n|^)\s*&/g, '\n>')
-      .replace(/(\n|^)>+/g, '\n>');
+    return text.replace(/^(\s*)&/gm, '>') // Citatos iš &
+      .replace(/^>+/gm, '>'); // Perteklinius > pašaliname
   }
 
   handleEmphasis(text) {
     return text
-      .replace(/(\w)_(\w)/g, '$1\\_$2')
-      .replace(/(^|\s)_([^_]+?)_(\s|$)/g, '$1*$2*$3')
-      .replace(/\*\*(.*?)\*\*/g, '**$1**');
+      // Pirmiausia konvertuojame __bold__ į **bold**
+      .replace(/__([^_]+)__/g, '**$1**')
+      // Tada _emphasis_ su bet kokiais ribotais simboliais
+      .replace(/(\W|^)_([^_]+)_(\W|$)/g, '$1*$2*$3')
+      // Escape'iname likusius _ ženklus žodžiuose
+      .replace(/(\w)_(\w)/g, '$1\\_$2');
   }
 
   cleanWhitespace(text) {
